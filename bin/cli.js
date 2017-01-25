2145f0bc91b8de10b1370db4f67b5f5a391bdbbd
YuHeng Sun <butsalt@163.com>
2017-01-25 13:35:25 +0800

    add liftoff

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-require('../index');
+var Liftoff = require('liftoff');
+
+var argv = require('minimist')(
+    process.argv.slice(2)
+);
+
+var Play = new Liftoff({
+    name: 'play'
+});
+
+Play.launch({
+
+}, function (env) {
+    //加载配置文件
+    if (env.configPath) {
+        require(env.configPath);
+    }
+});
